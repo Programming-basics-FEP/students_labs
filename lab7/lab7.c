bd47f02371caa61851d66d8a862722ee38fbc6e1
ArturLys <Artur.Lys@lnu.edu.ua>
2024-10-13 13:55:04 +0300

    lab7

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,77 +1,48 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 
-// 2: Порівняння двох чисел
-void comp_num(double a, double b) {
-    printf((a>b) ? "a > b\n" : "a <= b\n");
-}
-
-// 3: Визначення типу трикутника
-void tri_type(double a, double b, double c) {
-    if (a+b<=c || a+c<=b || b+c<=a)
-    {
-        printf("Не трикутник\n");
-        return;
+// 2. функція для перевірки чи число просте
+int task2(int a) {
+    if (a <= 1) return 0;
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i == 0) return 0;
     }
-        
-    char s_type[47];
-    if (a==b && b==c) strcpy(s_type, "Рівносторонній");
-    else if (a==b || b==c || a==c) strcpy(s_type, "Рівнобедрений");
-    else strcpy(s_type, "Різносторонній");
-
-    double sq[3] = {a*a, b*b, c*c};
-    if (sq[0]>sq[1]) { double temp = sq[0]; sq[0] = sq[1]; sq[1] = temp; }
-    if (sq[1]>sq[2]) { double temp = sq[1]; sq[1] = sq[2]; sq[2] = temp; }
-    if (sq[0]>sq[1]) { double temp = sq[0]; sq[0] = sq[1]; sq[1] = temp; }
-
-    char a_type[47];
-    if (sq[0]+sq[1]==sq[2]) 
-        strcpy(a_type, "прямокутний");
-    else if 
-        (sq[0]+sq[1]>sq[2]) strcpy(a_type, "гострокутний");
-    else 
-        strcpy(a_type, "тупокутний");
-
-    printf("Трикутник %s і %s\n", s_type, a_type);
+    return 1;
 }
 
-// 4: Перетворення числа на текст
-void num_to_words(int n, char* res) {
-    char* ones[] = {"", "один", "два", "три", "чотири", "п'ять", "шість", "сім", "вісім", "дев'ять"};
-    char* teens[] = {"десять", "одинадцять", "дванадцять", "тринадцять", "чотирнадцять", "п'ятнадцять", "шістнадцять", "сімнадцять", "вісімнадцять", "дев'ятнадцять"};
-    char* tens[] = {"", "", "двадцять", "тридцять", "сорок", "п'ятдесят", "шістдесят", "сімдесят", "вісімдесят", "дев'яносто"};
-    char* hundreds[] = {"", "Сто", "Двісті", "Триста", "Чотириста", "П'ятсот", "Шістсот", "Сімсот", "Вісімсот", "Дев'ятсот"};
+// 3. функція для обчислення суми чисел від A до 100
+int task3(int a) {
+    if (a > 100) return 0;
+    return a + task3(a + 1);
+}
 
-    int h = n/100, r = n%100, t = r/10, o = r%10;
-
-    if (n>100) sprintf(res, "%s ", hundreds[h]); 
-    if (r==0) return;
-    if (r<10) sprintf(res + strlen(res), "%s", ones[o]);
-    else if (r<20) sprintf(res + strlen(res), "%s", teens[r-10]);
-    else sprintf(res + strlen(res), "%s %s", tens[t], ones[o]);
+// 4. функція для знаходження кореня рівняння методом половинного ділення
+double task4(int N, int A, double l, double r) {
+    double m;
+    while ((r-l) >= 1e-5) {
+        m=l+(r-l)/2;
+        if (N*m+A==0) return m;
+        if (N*m+A>0) r=m;
+        else l=m; 
+    }
+    return m;
 }
 
 int main() {
-    // 2: Порівняння чисел
-    double a, b;
-    printf("Завдання 2: введіть два числа: ");
-    scanf("%lf %lf", &a, &b);
-    comp_num(a, b);
+    int a, N = 12, A = 17; // N - номер, A - вік
+    
+    // 1. Перевірка на простоту
+    printf("Введіть число: ");
+    scanf("%d", &a);
+    if (task2(a))
+        printf("%d є простим числом.\n", a);
+    else
+        printf("%d не є простим числом.\n", a);
+    
+    // 2. Рекурсивна сума
+    printf("Сума чисел від %d до 100: %d\n", N, task3(N));
 
-    // 3: Визначення типу трикутника
-    double x, y, z;
-    printf("Завдання 3: введіть сторони трикутника: ");
-    scanf("%lf %lf %lf", &x, &y, &z);
-    tri_type(x, y, z);
-
-    // 4: Перетворення тризначного числа на текст
-    int n;
-    char res[74];
-    printf("Завдання 4: введіть тризначне число: ");
-    scanf("%d", &n);
-    num_to_words(n, res);
-    printf("%s\n", res);
-
+    // 3. Метод половинного ділення
+    printf("Розв'язок рівняння %d*x + %d = 0: x ≈ %.8f\n", N, A, task4(N, A, -100, 100)); 
+    
     return 0;
 }
