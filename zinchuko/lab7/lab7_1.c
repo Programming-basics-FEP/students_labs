e58279fe5ffbfd0ea7167e8f4709b894a81ddccb
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 15:09:29 +0300

    Update lab7_1.c

--- zinchuko/lab7/lab7_1.c
+++ zinchuko/lab7/lab7_1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int number;
+    printf("Введіть число: ");
+    scanf("%d", &number);
+
+    if (isPrime(number)) {
+        printf("%d є простим числом.\n", number);
+    } else {
+        printf("%d не є простим числом.\n", number);
+    }
+
+    return 0;
+}
 
