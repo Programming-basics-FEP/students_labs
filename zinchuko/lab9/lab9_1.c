fc98d7c2a19395439127ea52b1a28280bda50134
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 15:42:25 +0300

    Update lab9_1.c

--- zinchuko/lab9/lab9_1.c
+++ zinchuko/lab9/lab9_1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseWord(char* word) {
+    int len = strlen(word);
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", word[i]);
+    }
+}
+
+int main() {
+    char sentence[256];
+    printf("Введіть прислів'я або афоризм (мінімум 7 слів):\n");
+    fgets(sentence, sizeof(sentence), stdin); 
+
+    char* word = strtok(sentence, " ");
+    while (word != NULL) {
+        reverseWord(word);
+        printf("\n");
+        word = strtok(NULL, " ");
+    }
+
+    return 0;
+}
 
