9f92b04e8ee67d7fa0a4f4c5262555a23bbc2e2b
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 15:09:49 +0300

    Update lab7_3.c

--- zinchuko/lab7/lab7_3.c
+++ zinchuko/lab7/lab7_3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int recursiveSum(int num) {
+    if (num > 100) return 0;
+    return num + recursiveSum(num + 1);
+}
+
+int main() {
+    int myNumber;
+    printf("Введіть ваш порядковий номер у журналі: ");
+    scanf("%d", &myNumber);
+
+    int sum = recursiveSum(myNumber);
+    printf("Сума чисел від %d до 100: %d\n", myNumber, sum);
+
+    return 0;
+}
 
