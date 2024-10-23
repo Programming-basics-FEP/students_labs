ab485ef86c82e11f0c3e30a1c3fa4bd4c31cd195
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 14:40:49 +0300

    Update lab6_2.c

--- zinchuko/lab6/lab6_2.c
+++ zinchuko/lab6/lab6_2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int sum = 0;
+    int i = 1;
+    int my_number = 9; // Замість 25 вставте свій порядковий номер
+
+    while (i <= 100) {
+        if (i != my_number && i != 33) {
+            sum += i;
+        }
+        i++;
+    }
+
+    printf("Сума: %d\n", sum);
+    return 0;
+}
