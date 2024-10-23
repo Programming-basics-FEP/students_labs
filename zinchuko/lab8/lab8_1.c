04437012c774b1645829e2ec30d2fe527bf35b48
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 15:10:34 +0300

    Update lab8_1.c

--- zinchuko/lab8/lab8_1.c
+++ zinchuko/lab8/lab8_1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateRandomArray(int arr[], int size) {
+  srand(time(0));
+  for (int i = 0; i < size; i++) {
+    arr[i] = rand() % 100;
+  }
+}
+
+int main() {
+  int arr[10];
+  generateRandomArray(arr, 10);
+
+  printf("Масив з 10 випадкових чисел:\n");
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
 
