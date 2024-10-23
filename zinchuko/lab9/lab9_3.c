02efe6a3455fd9bd952589d0fe15cc82be8f01d7
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-23 15:48:29 +0300

    Update lab9_3.c

--- zinchuko/lab9/lab9_3.c
+++ zinchuko/lab9/lab9_3.c
@@ -1,1 +1,43 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int isValidEmail(char *email) {
+  char *atPos = strchr(email, '@');
+  char *dotPos = strrchr(email, '.');
+
+  if (atPos == NULL || dotPos == NULL || atPos > dotPos) {
+    return 0;
+  }
+
+  if (atPos == email || *(atPos + 1) == '\0') {
+    return 0;
+  }
+
+  if (*(dotPos + 1) == '\0') {
+    return 0;
+  }
+
+  for (char *p = email; *p; p++) {
+    if (!isalnum(*p) && *p != '.' && *p != '@' && *p != '_') {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int main() {
+  char email[100];
+  printf("Введіть адресу електронної пошти: ");
+  scanf("%s", email);
+
+  if (isValidEmail(email)) {
+    printf("Адреса електронної пошти коректна.\n");
+  } else {
+    printf("Адреса електронної пошти некоректна.\n");
+  }
+
+  return 0;
+}
 
