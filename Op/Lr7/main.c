d2d03b770b5a1f5fa5248252279588229eca2cda
vlinear0 <vitalliy2005@gmail.com>
2024-11-07 17:34:18 +0200

    fix 9 lr

--- Op/Lr7/main.c
+++ Op/Lr7/main.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 bool is_prime(int n) {
-    if (n <= 1) return false;
+    if (n < 1) return false;
     if (n % 2 == 0) return (n == 2);
 
     for (int i = 3; i <= sqrt(n); i += 2) {
@@ -21,22 +21,22 @@ int req_sum(int ctr) {
     }
 }
 
-int f(int x) {
-    return 3 * x + 19;
+double f(double x) {
+    double N = 0;
+    double A = 19;
+    return (N==0) ? NAN : N * x + A;
 }
 
 double bisection_method(double from, double to) {
+    if (isnan(f(52))) return NAN;
     double mid;
     while ((to - from) > 1e-6) {
         mid = (from + to) / 2;
         if (f(mid) == 0) return mid;
-        if (f(mid) * f(from) < 0) {
-            to = mid;
-        } else {
-            from = mid;
-        }
+        if (f(mid) * f(from) < 0) to = mid;
+        else from = mid;
     }
-    return (from + to) / 2;
+    return mid;
 }
 
 int main() {
@@ -46,10 +46,9 @@ int main() {
     scanf("%d", &user);
     printf("Число %s ", (is_prime(user) ? "просте\n" : "не просте\n"));
     printf("---| З А В Д А Н Н Я  2 |---\n");
-    printf("Рекурсивна сума від 3 до 100: %d\n", req_sum(3));
+    printf("Рекурсивна сума від 10 до 100: %d\n", req_sum(10));
     printf("---| З А В Д А Н Н Я  3 |---\n");
-    int root = bisection_method(-100, 100);
-    if (root != -1) {
-        printf("Корінь: %f\n", (float)root);
-    }
+    double root = bisection_method(-100, 100);
+    if (!isnan(root)) printf("Корінь: %f\n", root);
+    else printf("Немає коренів!!!!!!");
 }
--- Op/Lr9/main.c
+++ Op/Lr9/main.c
@@ -1,48 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
+#include <regex.h>
 
-bool check_text(char* text) {
-    int sz = strlen(text);
-    int ctr = 0;
-    bool in_word = false;
 
-    for (int i = 0; i < sz; i++) {
-        if (text[i] == ' ') {
-            if (in_word) {
-                ctr++;
-                in_word = false;
-            }
-        } else {
-            in_word = true;
-        }
-    }
+typedef struct {
+    char* phone_number;
+    char* name;
+    char* surname;
+} User;
 
-    if (in_word) {
-        ctr++;
-    }
-
-    return ctr >= 7;
+char* input(char* str) {
+    char* inp = (char*)malloc(150*sizeof(char));
+    printf("(%s)$ ", str);
+    fgets(inp, 150, stdin);
+    inp[strlen(inp)] = '\n';
+    return inp;
 }
 
-void task_one() {
-    char* text = (char*)malloc(256 * sizeof(char));
+char* rstr(char* str) {
+    int ln = strlen(str);
+    char* ans = (char*)malloc((ln + 1) * sizeof(char));
+    for (int i = ln-1; i>=0; i--)
+    { ans[ln-i-1] = str[i]; }
+    return ans;
+}
 
-    printf("Уведіть прислів'я або афоризм, розділяючи кожне слово пробілом: ");
-    scanf(" %[^\n]", text);
+void showstr(char* str) {
+    printf("%s\n", str);
+}
 
-    while (!check_text(text)) {
-        printf("У тексті менше 7 слів!\nСпробуй ще раз: ");
-        scanf(" %[^\n]", text);
+User cmpuser(char* user) {
+    User result;
+    char* token;
+    int start = 0;
+
+    token = strtok(user, " ");
+    if (token != NULL) {
+        result.phone_number = token;
     }
 
-    
+    token = strtok(NULL, " ");
+    if (token != NULL) {
+        result.name = token;
+    }
 
-    free(text);
+    token = strtok(NULL, " ");
+    if (token != NULL) {
+        result.surname = token;
+    }
+
+    return result;
+}
+
+void task_one(char* str) {
+    int start = 0;
+    int ln = strlen(str);
+    for (int i = 0; i <= ln; i++) {
+        if (isspace(str[i])) {
+            if (i > start) {
+                char* tmp = (char*)malloc((i - start + 1) * sizeof(char));
+                strncpy(tmp, str + start, i - start);
+                showstr(rstr(tmp));
+                free(tmp);
+            }
+            start = i + 1;
+        }
+    }
+}
+
+void task_two(User* user) {
+    for(int i = 0; i<10; i++) {
+        
+    }
+}
+
+void task_three(char* mail) {
+    mail[strcspn(mail, "\n")] = '\0';
+    const char* pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    const char* string = mail;
+
+    regex_t regex;
+    int result;
+
+    result = regcomp(&regex, pattern, REG_EXTENDED);
+    result = regexec(&regex, string, 0, NULL, 0);
+    if (result == 0) {
+        printf("Це правильно введеня електронна пошта\n");
+    } else if (result == REG_NOMATCH) {
+        printf("Це неправильно введена електронна пошта\n");
+    }
+
+    regfree(&regex);
 }
 
 int main() {
-    task_one();
+    /// T A S K   O N E
+    char* w_input = input("Завадання 1");
+    task_one(w_input);
+    free(w_input);
+
+    /// T A S K   T W O
+    printf("(Завдання 2)\n");
+    User* u_input = (User*)malloc(15*sizeof(User));
+    for(int i = 0; i<10; i++) {
+        char* inp = (char*)malloc(100*sizeof(char));
+        printf("\t(%d) ", i+1);
+        fgets(inp, 100, stdin);
+        u_input[i] = cmpuser(inp);
+    }
+    task_two(u_input);
+    free(u_input);
+
+    /// T A S K   T H R E E
+    char* e_input = input("Завдання 3");
+    task_three(e_input);
+    free(e_input);
+
     return 0;
 }
