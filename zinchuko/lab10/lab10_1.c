7987c2fb8c9fd18aab4a29fda7e758409ff2d5f1
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-11-13 19:52:41 +0200

    Update lab10_1.c

--- zinchuko/lab10/lab10_1.c
+++ zinchuko/lab10/lab10_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int string_length(const char *str) {
+    int length = 0;
+    while (*str != '\0') {
+        length++;
+        str++;
+    }
+    return length;
+}
+
+int main() {
+    char str[] = "Hello, world!";
+    printf("Length of the string: %d\n", string_length(str));
+    return 0;
+}
 
