5e6647e3dc94f14666d70ab1e99e9fab553f7163
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-11-13 19:53:03 +0200

    Update lab10_2.c

--- zinchuko/lab10/lab10_2.c
+++ zinchuko/lab10/lab10_2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* toPoint(int x, int y) {
+    int *point = (int*)malloc(2 * sizeof(int)); // Виділення пам'яті для двох цілих чисел
+    if (point == NULL) {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+    point[0] = x; // Збереження координати x
+    point[1] = y; // Збереження координати y
+    return point;
+}
+
+int main() {
+    int *point = toPoint(5, 10);
+    if (point != NULL) {
+        printf("Point coordinates: (%d, %d)\n", point[0], point[1]);
+        printf("Pointer address: %p\n", (void*)point);
+        free(point); // Звільнення пам'яті
+    }
+    return 0;
+}
 
